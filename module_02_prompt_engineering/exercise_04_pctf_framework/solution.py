d69bbcad624e5afba8dd2a8fb903c0aa88d47aa5
Alexandros Polymeros <alpolymer@gmail.com>
2026-04-23 20:17:58 +0300

    feat: add exercise_04_pctf_framework solution

---
+++ module_02_prompt_engineering/exercise_04_pctf_framework/solution.py
@@ -0,0 +1,35 @@
+from utils import ask
+
+
+def create_pctf_prompt(persona:str, context:str, task: str, format:str) -> tuple[str, str]:
+
+    system_prompt = persona.strip()
+
+    user_prompt = f""" Context: {context.strip()}
+
+Task: {task.strip()}
+
+format: {format.strip()}
+"""
+    
+    return system_prompt, user_prompt
+
+my_persona = "You are an expert in machine learning and LLM's.You can break down and explain complex concepts with clarity and simplicity to non-technical stakeholders"
+
+my_context = "We are a company that tries to persuade our investors for the positive potential of using AI and LLM's in various sectors of our production"
+
+my_task = "Describe without technical jargon to our investors who are not technical persons the builiding blocks and mechanisms of LLM's"
+
+my_format = "Reply with  a numbered list of these mechanisms with the corresponding explanation below.Use Markdown"
+
+
+system_promt, user_prompt = create_pctf_prompt(
+    persona= my_persona,
+    context= my_context,
+    task= my_task,
+    format= my_format
+)
+
+response = ask(prompt=user_prompt, system=system_promt, max_tokens=600)
+
+print(response)
